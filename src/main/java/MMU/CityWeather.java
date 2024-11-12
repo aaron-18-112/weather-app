d1e4c7d238bc11b6b157858522d8f840bbb26cd5
aaron-18-112 <aaron.shah.18.112@gmail.com>
2024-11-12 11:19:55 +0000

    Formatted display weather, and commented out code

--- src/main/java/MMU/CityWeather.java
+++ src/main/java/MMU/CityWeather.java
@@ -48,12 +48,15 @@ public class CityWeather {
     }
 
     //Displays weather data in a readable format
+    //Implemented printf in order to align text
+    //%s inserts a string, %.2f inserts a double (2dp), %d inserts an integer
+    //%% inserts a literal %, %n inserts a new line after the text, \t aligns all the :
 
     public void displayWeather() {
-        System.out.println("City Name: " + cityName);
-        System.out.println("Temperature: " + temperature + "C");
-        System.out.println("Humidity: " + humidity + "%");
-        System.out.println("Description: " + description);
-        System.out.println("Forecast: " + forecast);
+        System.out.printf("City Name\t: %s%n", cityName);
+        System.out.printf("Temperature\t: %.2fC%n", temperature);
+        System.out.printf("Humidity\t: %d%%%n", humidity);
+        System.out.printf("Description\t: %s%n", description);
+        System.out.printf("Forecast\t: %s%n", forecast);
     }
 }
